0e721e83c20bf878c3effd5138fe3819963f2713
ck100pro <ck100protf10@gmail.com>
2019-12-14 21:39:43 +0800

    add_devise_controller_and_upload_user_avatar
    新增devise的controller和上傳圖片功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #使用者頭像圖片欄
+  has_one_attached :user_avatar
+
 end
