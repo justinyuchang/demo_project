db7fe2e8ea990030076b29899247890f39a0788a
ck100pro <ck100protf10@gmail.com>
2019-12-16 22:26:17 +0800

    fix_all_role
    修正分頁內的多行重複狀況

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+                :recoverable, :rememberable, :validatable,
+                :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   #使用者頭像圖片欄
   has_one_attached :user_avatar
+
+  def self.from_google_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :boards
   resources :lists
   resources :cards
-  devise_for :users
   devise_for :users, controllers: {
     sessions: "users/sessions",
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
