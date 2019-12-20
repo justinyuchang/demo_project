19cb6bdcdc713c9a2420859bec443d212746ad18
ck100pro <ck100protf10@gmail.com>
2019-12-20 17:53:45 +0800

    fix_actioncable_channel.js
    畫面同步更新actioncable_channel.js

--- app/javascript/channels/boards_channel.js
+++ app/javascript/channels/boards_channel.js
@@ -3,13 +3,17 @@ import consumer from "./consumer"
 $( document ).on('turbolinks:load', function() {
     $(function(){
         let room_id =$("#1").data("id")
-        console.log(room_id)
+        let $element = $('[data-role = "message"]');
+        let messageTemplate = $('[data-role = "message-template"]');
+
         consumer.subscriptions.create(
-          {channel: "BoardsChannel",
-            board: room_id},
+          {channel: "BoardsChannel",board: room_id},
           {received: function(data) {
-            console.log("已連線")
-            console.log(data)
+            let content = messageTemplate.children().clone(true, true);
+            content.find('[data-role = "message-p"]').text(data.message);
+            content.find('[data-role = "message-em"]').text(data.user_id);
+            $element.append(content);
+            // $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000) 
           }
         }  
       )
