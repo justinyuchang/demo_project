bc532ed07d909eeea43faf87ae15ec0084ca84e7
ck100pro <ck100protf10@gmail.com>
2019-12-26 18:29:53 +0800

    fix_actioncable_connect
    修正actioncable的連接設定

--- app/javascript/channels/boards_channel.js
+++ app/javascript/channels/boards_channel.js
@@ -1,25 +1,18 @@
 import consumer from "./consumer"
 
 $( document ).on('turbolinks:load', function() {
-    $(function(){
-        let room_id =$("#1").data("id")
-        let $element = $('[data-role = "message"]');
-        let messageTemplate = $('[data-role = "message-template"]');
+    $(function(env){
+        let room_url = location.pathname.split('/')
+        let room_id =  room_url[room_url.length - 1]
 
         consumer.subscriptions.create(
           {channel: "BoardsChannel",board: room_id},
           {received: function(data) {
             console.log(data)
-            let lists = $('[data-role="lists-clone"]').clone(true, true)
-            lists.find('[data-role="lists"]').text(data.title)
-            lists.find('[data-role="lists"]').addClass("btn btn-primary")
-            $('[data-role= "list-item"]').append(lists)
+            let lists = $('[data-role="cart-clone"]').clone(true, true)
+            lists.find('[data-role="cart-title"]').text(data.title)
+            $('[data-role="lists-create"]').before(lists)
 
-
-            // let content = messageTemplate.children().clone(true, true);
-            // content.find('[data-role = "message-p"]').text(data.message);
-            // content.find('[data-role = "message-em"]').text(data.user_id);
-            // $element.append(content);
             // $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000) 
           }
         }  
