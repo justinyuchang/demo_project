db4a2afba08f4492648502de278ed48036f405c5
ck100pro <ck100protf10@gmail.com>
2020-01-04 00:11:24 +0800

    list_create_reset
    新增list的方式改寫,避免js事件問題

--- app/javascript/script/list.js
+++ app/javascript/script/list.js
@@ -1,26 +1,5 @@
 import axios from 'helpers/axios';
 
 $(document).on("turbolinks:load", function(){
-//list_create
-    $('[data-role="js-list"]').on("click",'[data-role="btn list-create"]',function(event){
-      event.preventDefault();
-      const board_url = location.pathname.split('/')
-      const board_id =  board_url[board_url.length - 1]
-      let api_list_title = $('[data-role="api-list-title"]').val();
-      axios({
-          method: 'post',
-          url: `/boards/${board_id}/lists`,
-          data: {
-            list: {
-              title: api_list_title
-            }
-          }
-        })
-        .then(function(response){
-          let status = response.data.status
-          if(status == "ok"){
-            $('[data-role="api-list-title"]').val(' ')
-          }
-        })
-  })
+
 })
