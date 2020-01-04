ff70e8476a0c3b5d796d05ee650f76ccd1f6e1fd
ck100pro <ck100protf10@gmail.com>
2020-01-04 21:51:30 +0800

    sortable_create(二)
    建立雙向拖拉選單

--- app/javascript/script/sortable.js
+++ app/javascript/script/sortable.js
@@ -3,8 +3,9 @@ import axios from 'helpers/axios';
 $(document).on("turbolinks:load", function(){
   $( function() {
     $( '[data-role="card-group"]' ).sortable({
-      connectWith: "#card-sortable"
-    });
-    // 
+      connectWith: "#card-sortable",
+      connectWith: '[data-role="card-group"]'
+    }).disableSelection();
+
   } );
 })
