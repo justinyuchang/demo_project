5a332c45a348119c3d377f54802be283624e7431
Chang <justinyu711@gmail.com>
2020-01-05 22:01:41 +0800

    Resolve conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
                 :recoverable, :rememberable, :validatable,
                 :omniauthable, omniauth_providers: [:google_oauth2]
 
+
   # ActiveRecord關聯設定
   has_many :user_boards
   has_many :boards, through: :user_boards
