2b57411ea171af6c1ae3350be53f50b17aaa7bf7
ck100pro <ck100protf10@gmail.com>
2020-01-06 11:32:41 +0800

    sorttable_create(五)
    api抓取資料方式改寫，只抓取前後加自己的值

--- app/javascript/script/sortable.js
+++ app/javascript/script/sortable.js
@@ -3,23 +3,29 @@ import axios from 'helpers/axios';
 $(document).on("turbolinks:load", function(){
     $( '[data-role="js-list"]' ).on("mousemove" ,function(){
       $( '[data-role="card-group"]' ).sortable({
-        items : 'div:not(.sortable-hidden)',
         connectWith: "#card-sortable",
         connectWith: '[data-role="card-group"]',
+        cursor: "move",
+        items: 'div:not(.sortable-hidden)',
+        delay: 150,
+        revert: true,
         update: function(event, ui){
           let list_id = $(this).parents('[data-role="card-wrapper"]')
                                                .siblings('[data-role= "list-item"]')
                                                .find('[data-role="list-id"]')
                                                .attr("val") 
-          let card_all = $(this).sortable('toArray')
-          console.log(card_all)
-          // axios({
-          //   method: 'patch',
-          //   url: '/lists/cards/sort',
-          //   data: {card :$(this).sortable('toArray'),
-          //                list_id :list_id
-          //   }
-          // })
+          let card_id = $(ui.item[0]).attr("id")
+          let card_id_next  = $(ui.item[0]).next().attr("id")
+          let card_id_prev  = $(ui.item[0]).prev().attr("id")
+          axios({
+            method: 'patch',
+            url: '/lists/cards/sort',
+            data: {card: [{card_id: card_id},
+                                      {card_id_next: card_id_next},
+                                      {card_id_prev: card_id_prev}],
+                         list_id: list_id
+            }
+          })
         }
       })
     })
