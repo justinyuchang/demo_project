97f5dc0b41c898e8393512ff458f5cc84acc6f66
ck100pro <ck100protf10@gmail.com>
2020-01-08 17:16:01 +0800

    fix_db:migrate
    將db衝突解掉

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_01_07_103421) do
 
   # These are extensions that must be enabled in order to support this database
@@ -77,21 +76,6 @@ ActiveRecord::Schema.define(version: 2020_01_07_103421) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "labellings", force: :cascade do |t|
-    t.bigint "label_id", null: false
-    t.bigint "card_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["card_id"], name: "index_labellings_on_card_id"
-    t.index ["label_id"], name: "index_labellings_on_label_id"
-  end
-
-  create_table "labels", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.integer "position"
@@ -112,6 +96,15 @@ ActiveRecord::Schema.define(version: 2020_01_07_103421) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_search_users_on_board_id"
     t.index ["user_id"], name: "index_search_users_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "user_boards", force: :cascade do |t|
@@ -158,8 +151,6 @@ ActiveRecord::Schema.define(version: 2020_01_07_103421) do
   add_foreign_key "cards", "lists"
   add_foreign_key "comments", "cards"
   add_foreign_key "comments", "users"
-  add_foreign_key "labellings", "cards"
-  add_foreign_key "labellings", "labels"
   add_foreign_key "lists", "boards"
   add_foreign_key "search_users", "boards"
   add_foreign_key "search_users", "users"
