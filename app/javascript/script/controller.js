bc008d8f92b4db9af8bdded38e0539a58cd48f60
ck100pro <ck100protf10@gmail.com>
2020-01-10 10:27:23 +0800

    list_create_type
    更改list_create時的樣式

--- app/javascript/script/controller.js
+++ app/javascript/script/controller.js
@@ -1,10 +1,10 @@
 $(document).on("turbolinks:load", function(){
-    $(function(){
-        $("#js-list-from").hide()
-    })
-    $("#js-list-content-btn").on("click", function(){
-        $("#js-list-from").toggle()
-    })
+    // $(function(){
+    //     $("#js-list-from").hide()
+    // })
+    // $("#js-list-content-btn").on("click", function(){
+    //     $("#js-list-from").toggle()
+    // })
 
     // $("#list-container").on("click", function(){
     //     if ($("#js-list-from").css("display") == "block"){
