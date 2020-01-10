8ad8591d5a54505c062b2446eea082e29ecadb8e
ck100pro <ck100protf10@gmail.com>
2020-01-10 18:08:13 +0800

    card_create_controller
    list控制按鍵功能完成一部份

--- app/javascript/script/controller.js
+++ app/javascript/script/controller.js
@@ -1,14 +1,27 @@
 $(document).ready( function() {
   $(".parentDiv").on("click", function(event){
-    if($(event.target).hasClass("list-add-top")){
-        $(".list-add-form").toggle()
-          $(event.target).hide()
-      }else{
-        $(event.target).show("slow",function(){
-          $("#js-list-content-btn").show()
-          $(".list-add-form").hide()
-        })
+    if ($(event.target).hasClass("list-add-top")){
+      $(".list-add-form").toggle(function(){
+        $(".form-inp").focus()
+      })
+        $(event.target).hide()
+    } else if ($(event.target).parents().hasClass("list-wrapper")){
+      $(".form-inp").focus()
+    } else {
+      $(event.target).show("slow",function(){
+        $("#js-list-content-btn").show()
+        $(".list-add-form").hide()
+      })
     }
   })
-  //
+  
+  $(".parentDiv").on("click", function(){
+    let cardHasClass =$(event.target).hasClass("card-create-span")
+    if(cardHasClass){
+      $(event.target).parents(".card-wrapper").find(".card-input-controller").toggle()
+      $(event.target).parents(".card-wrapper").find(".card-create-area").focus()
+    } else {
+      $(".card-input-controller").hide()
+    }
+  })
 })
