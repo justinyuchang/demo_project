5e09845ad20bd39c50649a2874f9f403e74b3147
ck100pro <ck100protf10@gmail.com>
2020-01-10 14:36:13 +0800

    list_create_controller_finish
    list控制按鍵功能完成

--- app/javascript/script/controller.js
+++ app/javascript/script/controller.js
@@ -1,17 +1,14 @@
-$(document).on("turbolinks:load", function(){
-    // $(function(){
-    //     $("#js-list-from").hide()
-    // })
-    // $("#js-list-content-btn").on("click", function(){
-    //     $("#js-list-from").toggle()
-    // })
-
-    // $("#list-container").on("click", function(){
-    //     if ($("#js-list-from").css("display") == "block"){
-    //         console.log("efwblu")
-    //         // $("#js-list-from").hide()
-    //     }else{
-    //         return true
-    //     }
-    // })
+$(document).ready( function() {
+  $(".parentDiv").on("click", function(event){
+    if($(event.target).hasClass("list-add-top")){
+        $(".list-add-form").toggle()
+          $(event.target).hide()
+      }else{
+        $(event.target).show("slow",function(){
+          $("#js-list-content-btn").show()
+          $(".list-add-form").hide()
+        })
+    }
+  })
+  //
 })
