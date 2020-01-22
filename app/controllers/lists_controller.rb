c809d235f3ced5dc1eadf361bc6b6beae3b3133f
ck100pro <ck100protf10@gmail.com>
2020-01-23 00:23:47 +0800

    soerable_restructuring(一)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,25 +17,25 @@ class ListsController < ApplicationController
   end 
 
   def sortlist
-    next_list = List.find_by(id: params[:next_list_id]) || nil
-    prev_list = List.find_by(id: params[:prev_list_id]) || nil
-    if (prev_list.blank?) 
+    @next_list = List.find_by(id: params[:next_list_id]) || nil
+    @prev_list = List.find_by(id: params[:prev_list_id]) || nil
+    if (@prev_list.blank?) 
       @list.move_to_top
-      list_add_prev = {list: @list.id, next_id: next_list.id, status: "list_add_prev"}
+      @list_add_prev = {list: @list.id, next_id: @next_list.id, status: "list_add_prev"}
       BoardsChannel.broadcast_to(@board, list_add_prev)
-    elsif (next_list.blank?)
+    elsif (@next_list.blank?)
       @list.move_to_bottom
-      list_add_next = {list: @list.id, prev_id: prev_list.id, status: "list_add_next"}
-      BoardsChannel.broadcast_to(@board, list_add_next)
+      @list_add_next = {list: @list.id, prev_id: @prev_list.id, status: "list_add_next"}
+      BoardsChannel.broadcast_to(@board, @list_add_next)
     else
-      if ( @list.position <  prev_list.position )
-        @list.insert_at(prev_list.position)
-        list_add_next = {list: @list.id, prev_id: prev_list.id, status: "list_add_next"}
-        BoardsChannel.broadcast_to(@board, list_add_next)
+      if ( @list.position < @prev_list.position )
+        @list.insert_at(@prev_list.position)
+        @list_add_next = {list: @list.id, prev_id: @prev_list.id, status: "list_add_next"}
+        BoardsChannel.broadcast_to(@board, @list_add_next)
       else
-        @list.insert_at(next_list.position)
-        list_add_next = {list: @list.id, prev_id: prev_list.id, status: "list_add_next"}
-        BoardsChannel.broadcast_to(@board, list_add_next)
+        @list.insert_at(@next_list.position)
+        @list_add_next = {list: @list.id, prev_id: @prev_list.id, status: "list_add_next"}
+        BoardsChannel.broadcast_to(@board, @list_add_next)
       end
     end
   end
