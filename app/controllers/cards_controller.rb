ac2ee9f6a25b555fc6d0689b29ae726e4394c46c
ck100pro <ck100protf10@gmail.com>
2020-01-24 14:58:26 +0800

    soerable_restructuring(三)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   before_action :find_board, only: [:create, :sortcard]
+  before_action :find_card_sortable, only: [:sortcard]
   before_action :load_card_items_params, only: [:update]
   
   def show
@@ -13,9 +14,6 @@ class CardsController < ApplicationController
     BoardsChannel.broadcast_to(@board, @card_channel)
     render json: {status: 200}
   end
-  
-  def edit 
-  end 
   
   def update 
     @find_card.update(@card_item_params)
@@ -39,53 +37,65 @@ class CardsController < ApplicationController
   end 
 
   def sortcard
-    @find_list = List.find(params[:list_id])
-    @find_card = Card.find_by(id: params[:card])
-    @find_card_array = params[:card_array]
-    @next_card = Card.find_by(id: params[:next_card_id]) || nil
-    @prev_card = Card.find_by(id: params[:prev_card_id]) || nil
     if (@find_card.list_id == @find_list.id)
-      if @prev_card.blank?
-        @find_card.move_to_top
-        @card_add_prev = {list_id: @find_list.id, card_id: @find_card, next_id: @next_card.id, status: "card_add_prev"}
-        BoardsChannel.broadcast_to(@board, @card_add_prev)
-      elsif @next_card.blank?
-        @find_card.move_to_bottom
-        @card_add_next = {list_id: @find_list.id, card_id: @find_card, prev_id: @prev_card.id, status: "card_add_next"}
-        BoardsChannel.broadcast_to(@board, @card_add_next)
-      else
-        if ( @find_card.position < @prev_card.position )
-          @find_card.insert_at(@prev_card.position)
-          @card_add_prev = {list_id: @find_list.id, card_id: @find_card, next_id: @next_card.id, status: "card_add_prev"}
-          BoardsChannel.broadcast_to(@board, @card_add_prev)
-        else
-          @find_card.insert_at(@next_card.position)
-          @card_add_next = {list_id: @find_list.id, card_id: @find_card, prev_id: @prev_card.id, status: "card_add_next"}
-          BoardsChannel.broadcast_to(@board, @card_add_next)
-        end
-      end
+      card_at_list
     else
-      if (@prev_card.blank?) && (@next_card.blank?)
-        @find_card.update(list_id: @find_list.id )
-        @card_add = {list_id: @find_list.id, card_id: @find_card, status: "card_add"}
-        BoardsChannel.broadcast_to(@board, @card_add)
-      elsif (@prev_card.blank?)
-        @find_card.update(list_id: @find_list.id )
-        @find_card.move_to_top
-        @card_add_prev = {list_id: @find_list.id, card_id: @find_card, next_id: @next_card.id, status: "card_add_prev"}
-        BoardsChannel.broadcast_to(@board, @card_add_prev)
-      elsif (@next_card.blank?)
-        @find_card.update(list_id: @find_list.id )
-        @find_card.move_to_bottom
-        @card_add_next = {list_id: @find_list.id, card_id: @find_card, prev_id: @prev_card.id, status: "card_add_next"}
-        BoardsChannel.broadcast_to(@board, @card_add_next)
-      else
-        @find_card.update(list_id: @find_list.id )
-        @find_card.insert_at(@next_card.position)
-        @card_add_next = {list_id: @find_list.id, card_id: @find_card, prev_id: @prev_card.id, status: "card_add_next"}
-        BoardsChannel.broadcast_to(@board, @card_add_next)
-      end
+      card_not_at_list
     end
+  end
+
+  def card_at_list
+    if @prev_card.blank?
+      card_prev_nil
+    elsif @next_card.blank?
+      card_next_nil
+    else
+      card_not_nil
+    end
+  end
+
+  def card_not_at_list
+    @find_card.update(list_id: @find_list.id)
+    if (@prev_card.blank?) && (@next_card.blank?)
+      card_add = {list_id: @find_list.id, card_id: @find_card, status: "card_add"}
+      BoardsChannel.broadcast_to(@board, card_add)
+    elsif (@prev_card.blank?)
+      card_prev_nil
+    elsif (@next_card.blank?)
+      card_next_nil
+    else
+      @find_card.insert_at(@next_card.position)
+      card_add_after
+    end
+  end
+
+  def card_prev_nil
+    @find_card.move_to_top
+    card_add_front
+  end
+
+  def card_next_nil
+    @find_card.move_to_bottom
+    card_add_after
+  end
+
+  def card_not_nil
+    if ( @find_card.position < @prev_card.position )
+      @find_card.insert_at(@prev_card.position)
+    else
+      @find_card.insert_at(@next_card.position)
+    end
+    card_add_front
+  end
+
+  def card_add_front
+    add_front = {list_id: @find_list.id, card_id: @find_card, next_id: @next_card.id, status: "card_add_prev"}
+    BoardsChannel.broadcast_to(@board, add_front)
+  end
+
+  def card_add_after
+    add_after = {list_id: @find_list.id, card_id: @find_card, prev_id: @prev_card.id, status: "card_add_next"}
+    BoardsChannel.broadcast_to(@board, add_after)
   end
 
   def tagging
@@ -103,6 +113,14 @@ class CardsController < ApplicationController
   private
   def find_board
     @board = Board.find(params[:board_id])
+  end
+
+  def find_card_sortable
+    @find_list = List.find(params[:list_id])
+    @find_card = Card.find_by(id: params[:card])
+    @next_card = Card.find_by(id: params[:next_card_id]) || nil
+    @prev_card = Card.find_by(id: params[:prev_card_id]) || nil
+    @find_card_array = params[:card_array]
   end
 
   def card_params
